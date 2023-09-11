85f5757f18d00e3eceacfde5495b7ca97f3d7f7c
olenakalychak <144223584+wiuiaus@users.noreply.github.com>
2023-09-11 16:55:40 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+let nul = NaN
+let und = "undefined"
+let	num = 1
+let text = "ukd"
+let t = true
+
+
+let i = {
+	name: "Maria",
+	sur: "Smalukh",
+	age: 17,
+	from: "Ukraine"
+}
+console.log(nul, und, num, text, t)
+console.table(i)
